779bfe03b3366dd99bb569101f3d738eaca7d3ad
Maxime Thirouin <m@moox.io>
2014-07-18 07:15:18 +0200

    More robust watch for linting

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,22 @@
 var gulp = require("gulp")
+var util = require("gulp-util")
+var plumber = require("gulp-plumber")
 var jscs = require("gulp-jscs")
 var jshint = require("gulp-jshint")
+var watch = false
+
+var files = ["index.js", "test/**/*.js"]
 
 // lint
 gulp.task("default", function() {
-  return gulp.src("index.js")
+  return gulp.src(files)
+    .pipe(watch ? plumber() : util.noop())
     .pipe(jscs())
     .pipe(jshint())
     .pipe(jshint.reporter("jshint-stylish"))
 })
 
 gulp.task("watch", function() {
-  gulp.watch("./index.js", ["default"])
+  watch = true
+  gulp.watch(files, ["default"])
 })
